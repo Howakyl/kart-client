5b2fc8300ff05650049a23621170a57d42bf8086
Howakyl96 <howakyl@gmail.com>
2021-04-19 15:34:43 -0700

    query for all items, all items rendered on ItemList component

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { RenderItems } from '../graphql/queries/RenderItems';
+
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Typography, List, ListItem, ListItemText, ListItemIcon } from '@material-ui/core';
-import  FolderIcon  from '@material-ui/icons/Folder';
+import AddBoxIcon from '@material-ui/icons/AddBox';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,17 +20,31 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
-function generate(element) {
-  return [0, 1, 2].map((value) =>
-    React.cloneElement(element, {
-      key: value,
-    }),
-  );
-}
-
 const ItemList = () => {
   const classes = useStyles();
+  const { loading, error, data } = useQuery(RenderItems);
+  if (data) {
+    console.log(data)
+  }
+
+  function generateItems () {
+    return data.items.map(item => (
+      <ListItem key={item.id}>
+        <ListItemText primary={item.name}/>
+        <ListItemIcon>
+          <AddBoxIcon />
+        </ListItemIcon>
+      </ListItem>
+    ))
+  }
+
+  if (loading) {
+    return <p>Loading.... </p>
+  }
+  
+  if (error) {
+    return <p>Error :{error}</p>
+  }
 
   return (
     <Grid item xs={12} md={6} >
@@ -35,17 +52,8 @@ const ItemList = () => {
         All Items
       </Typography>
       <div className={classes.demo}>
-        <List >
-          {generate(
-            <ListItem>
-              <ListItemText
-                primary="Single-line item"
-              />
-              <ListItemIcon>
-                <FolderIcon />
-              </ListItemIcon>
-            </ListItem>
-          )}
+        <List>
+          {generateItems()}
         </List>
       </div>
     </Grid>
