d65926a0b8860b8df34dedfc9a1ae6f1a9e3d0ab
Howakyl <howakyl@gmail.com>
2021-04-25 12:53:13 -0700

    imported RenderItems, loading and error handling for rendering

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { } from '../graphql/queries/RenderItems';
 
 
 const Search = () => {
+  const { loading, error, data } = useQuery(RenderItems)
+  if (data) {
+    console.log(data)
+  }
+  if (loading) {
+    return <p>Loading...</p>
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>
+  }
+
   return (
     <div>
       <input type="text" placeholder="Search..." />
